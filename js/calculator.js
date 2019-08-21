29dc45a8a124dd83abe8fdc614cbeab318728052
andresger <andres.gerz@gmail.com>
2019-08-21 13:59:53 -0300

    I resolved the errors of multiply() and subtraction() and I changed variable's names

--- js/calculator.js
+++ js/calculator.js
@@ -1,49 +1,111 @@
 
-var digit = "";
-var accumulate = 0;
-var addition = false; 
-var subtract = false;
+let digit = 0;
+let accumulate = 0;
+let adding = false; 
+let subtracting = false;
+let multiplying = false;
+let p_operation = true;
 
-function display_numbers(n){
-  document.getElementById("display").value=digit+n;
+function display_numbers(n) {
+  document.getElementById("display").value = digit + n;
 
   digit = document.getElementById("display").value;
-}
+  console.log(digit);
+};
 
-function equals(){
+function equals() {
   
-  accumulate = accumulate + parseInt(digit);
-  document.getElementById("display").value=accumulate;
+  accumulate = accumulate + parseFloat(digit);
+  document.getElementById("display").value = accumulate;
+  console.log(accumulate);
+  digit = 0;
 
-  digit="";
+  adding = true;
+  subtracting = false;
+  multiplying = false;
+};
 
-  addition = true;
-  subtract = false;
+function subtraction() {
+
+  if(p_operation == false) {
+
+    if(adding) {
+      accumulate = accumulate + parseFloat(digit);
+      console.log(accumulate);
+      document.getElementById("display").value = accumulate;
+
+    } else {
+      accumulate = accumulate - parseFloat(digit);
+      console.log(accumulate);
+      document.getElementById("display").value = accumulate;
+    };
+  } else {
+    accumulate = parseFloat(digit);
+    console.log(accumulate);
+    p_operation = false;
+  };
+
+  digit = 0;
+  adding = false;
+  subtracting = true; 
+  multiplying = false;
+};
+
+function addition() {
+
+  if(subtract) {
+    accumulate = accumulate - parseFloat(digit);
+    console.log(accumulate);
+    document.getElementById("display").value = accumulate;
+
+  } else {
+    accumulate = accumulate + parseFloat(digit);
+    console.log(accumulate);
+    document.getElementById("display").value = accumulate;
+  };
+
+  digit = 0;
   
-}
+  adding= true;
 
-function substraction(){
+  subtracting = false;
+  multiplying = false;
+  p_operation = false;
+};
 
-  if(addition){
-    accumulate = accumulate + parseInt(digit);
-    document.getElementById("display").value=accumulate;
-  }else{
-    accumulate = accumulate - parseInt(digit);
-    document.getElementById("display").value=accumulate;
-  }
-  digit="";
-  addition=false;
-  subtract=true; 
-}
+function ac() {
+  digit = 0;
+  accumulate = 0;
+  console.log(accumulate + " -- " + digit);
+  document.getElementById("display").value = digit;
+}; 
 
-function result(){
 
-  if(addition){
-    document.getElementById("display").value=accumulate+parseInt(digit);
-  }else if(subtract){
-    document.getElementById("display").value=accumulate+parseInt(digit);
-  }
+function multiply() {
+  accumulate = accumulate + (1 * parseFloat(digit));
+  document.getElementById("display").value = accumulate;
+  console.log(accumulate);
+  digit = 0;
+  multiplying = true;
+  adding = false;
+  subtracting = false;
+};
 
-  accumulate=parseInt(document.getElementById("display").value);
-  digit=0;
+
+function result() {
+console.log(multiply);
+  if (adding) {
+    document.getElementById("display").value = accumulate + parseFloat(digit);
+
+  } else if (subtracting) {
+    document.getElementById("display").value = accumulate - parseFloat(digit);
+
+  } else if (multiplying) {
+    document.getElementById("display").value = accumulate * parseFloat(digit);
+  };
+
+  accumulate = parseFloat(document.getElementById("display").value);
+  digit = 0;
+  console.log(accumulate + " -- " + digit);
+  
 }
